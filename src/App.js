713e79c33b68087cf9ef6a7573addb648a58c7ea
Tregan <tregan@tregan.me>
2021-07-15 09:51:48 -0700

    feat: add wiki search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const searchTimeoutRef = useRef();
+  useEffect(() => {
+    if (searchTimeoutRef.current) {
+      clearTimeout(searchTimeoutRef.current);
+    }
+    searchTimeoutRef.current = setTimeout(() => {
+      console.log({ search });
+      if (!search) return;
+
+      doSearch(search);
+    }, 500);
+  }, [search]);
+
+  const [imageUrls, setImageUrls] = useState([]);
+  async function doSearch(query) {
+    try {
+      const url = `https://api.wikimedia.org/core/v1/commons/search/page?q=${encodeURIComponent(
+        query
+      )}`;
+
+      const response = await fetch(url).then((r) => r.json());
+      console.log({ response });
+      setImageUrls(
+        response.pages.map((r) => ({
+          url: r?.thumbnail?.url,
+          id: r?.id,
+        }))
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Wikimedia image browser
-        </h1>
+        <input
+          placeholder="Search images"
+          value={search}
+          onChange={(a) => setSearch(a.currentTarget.value)}
+        />
+        <h1>Wikimedia image browser</h1>
+        {imageUrls.map((image) => (
+          <img alt={search} src={image.url} key={image.id} />
+        ))}
       </header>
     </div>
   );
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,15 @@
 /* global cy */
-import * as React from 'react'
-import { mount } from '@cypress/react'
-import App from './App'
+import * as React from 'react';
+import { mount } from '@cypress/react';
+import App from './App';
 
 it('renders the header', () => {
-  mount(<App />)
-  cy.contains(/Wikimedia image browser/i)
-})
+  mount(<App />);
+  cy.contains(/Wikimedia image browser/i);
+});
+
+it('searches for images', () => {
+  mount(<App />);
+  cy.get('input').click().type('dog');
+  cy.get('img').should('exist');
+});
